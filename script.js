2034567b40e74fc413caab7aa0bf74de418e0a41
Sid2169 <siddharthasingh21109572@gmail.com>
2025-01-30 17:17:49 +0530

    Add functionality to change 'displayStr' variable whenevr the input field of id='displayText' is updated

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 /* --------------------- GLOBAL STRINGS --------------------- */
 // displayStr: what the user sees in the input field
-let displayStr = "";  
+// update displayStr every time input field value is updated
+
+let displayStr = "";
+document.getElementById("displayText").addEventListener('input', () => {
+    displayStr = document.getElementById("displayText").value;
+})
+ 
 /* 
   -- UNDO BUTTON --
   Removes the last character from the displayed expression.
@@ -9,4 +15,3 @@ document.getElementById("undo").addEventListener("click", () => {
     displayStr = displayStr.slice(0, -1);
     displayText.value = displayStr || "0";
   });
-  
